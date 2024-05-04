14f1e48b86c08d9204a62673ff748040f12cec1b
Lucifer <luciferxhnh@gmail.com>
2024-05-04 20:33:48 +0700

    done csrf

--- src/main/java/ori/controller/NotFoundUrl.java
+++ src/main/java/ori/controller/NotFoundUrl.java
@@ -1,19 +1,19 @@
-//package ori.controller;
-//
-//import org.springframework.boot.web.servlet.error.ErrorController;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import ch.qos.logback.core.model.Model;
-//
-//@Controller
-//public class NotFoundUrl implements ErrorController {
-//	@RequestMapping("/error")
-//    public String handleError() {
-//        return "404";
-//    }
-//
-//    public String getErrorPath(Model model) {
-//        return "redirect:/error";
-//    }
-//}
+package ori.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+public class NotFoundUrl implements ErrorController {
+	@RequestMapping("/error")
+    public String handleError() {
+        return "404";
+    }
+
+    public String getErrorPath(Model model) {
+        return "redirect:/error";
+    }
+}
